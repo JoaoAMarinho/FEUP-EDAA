3e53dbfce9a96aa8d6e9494b95f459613e60dd97
JoaoMarinho <61231873+JoaoAMarinho@users.noreply.github.com>
2023-04-17 12:08:52 +0100

    linter test

--- src/color.py
+++ src/color.py
@@ -11,6 +11,8 @@ class Color(object):
         self.green = green
         self.blue = blue
         self.alpha = alpha
+        pel = 10
+        oedkeodkedokedokedoeokd = 100 * 100000000 + 12121212 + pel - 99999999999999999999999999999999999999999999999999999999999999999999999999
 
     def __str__(self):
         return "{self.red}-{self.green}-{self.blue}"
